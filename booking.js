83553efb8c6f4d261103cdab1c17c7482dea13f4
Azienflv <121687950+Azienflv@users.noreply.github.com>
2026-04-09 19:46:05 -0400

    Update booking.js

--- booking.js
+++ booking.js
@@ -1,534 +1,14 @@
-const toursData = {
-  "saona-classic": {
-    title: "Saona Classic",
-    basePriceLabel: "From $89 USD",
-    adultPrice: 89,
-    childPrice: 45,
-    times: ["7:00 AM", "8:00 AM", "9:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "saona-vip": {
-    title: "Saona VIP",
-    basePriceLabel: "From $109 USD",
-    adultPrice: 109,
-    childPrice: 55,
-    times: ["7:00 AM", "8:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "saona-luxury": {
-    title: "Saona Luxury",
-    basePriceLabel: "From $139 USD",
-    adultPrice: 139,
-    childPrice: 70,
-    times: ["7:00 AM", "8:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-    "buggies-classic": {
-    title: "Buggies Classic",
-    basePriceLabel: "From $65 USD",
-    adultPrice: 65,
-    childPrice: 40,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "buggies-prime": {
-    title: "Buggies Prime",
-    basePriceLabel: "From $79 USD",
-    adultPrice: 79,
-    childPrice: 45,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "buggies-night": {
-    title: "Buggies Night",
-    basePriceLabel: "From $89 USD",
-    adultPrice: 89,
-    childPrice: 50,
-    times: ["6:00 PM", "7:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "buggies-blast": {
-    title: "Buggies Blast",
-    basePriceLabel: "From $95 USD",
-    adultPrice: 95,
-    childPrice: 55,
-    times: ["8:00 AM", "11:00 AM", "3:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-    "samana-cayo-levantado": {
-    title: "Samaná & Cayo Levantado",
-    basePriceLabel: "From $110 USD",
-    adultPrice: 110,
-    childPrice: 65,
-    times: ["6:30 AM", "7:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "samana-cayo-limon": {
-    title: "Samaná + Cayo + El Limón",
-    basePriceLabel: "From $125 USD",
-    adultPrice: 125,
-    childPrice: 75,
-    times: ["6:30 AM", "7:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "samana-cayo-ballenas": {
-    title: "Samaná + Whale Watching",
-    basePriceLabel: "From $135 USD",
-    adultPrice: 135,
-    childPrice: 80,
-    times: ["6:00 AM", "6:30 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-    "monkey-land": {
-    title: "Monkey Land",
-    basePriceLabel: "From $75 USD",
-    adultPrice: 75,
-    childPrice: 45,
-    times: ["8:00 AM", "1:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "double-adventure": {
-    title: "Double Adventure",
-    basePriceLabel: "From $99 USD",
-    adultPrice: 99,
-    childPrice: 60,
-    times: ["8:00 AM", "1:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "triple-adventure": {
-    title: "Triple Adventure",
-    basePriceLabel: "From $129 USD",
-    adultPrice: 129,
-    childPrice: 75,
-    times: ["8:00 AM", "1:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "cocobongo-regular": {
-    title: "Coco Bongo Regular",
-    basePriceLabel: "From $75 USD",
-    adultPrice: 75,
-    childPrice: 75,
-    times: ["8:00 PM", "9:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "cocobongo-gold-member": {
-    title: "Coco Bongo Gold Member",
-    basePriceLabel: "From $95 USD",
-    adultPrice: 95,
-    childPrice: 95,
-    times: ["8:00 PM", "9:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "cocobongo-front-row": {
-    title: "Coco Bongo Front Row",
-    basePriceLabel: "From $130 USD",
-    adultPrice: 130,
-    childPrice: 130,
-    times: ["8:00 PM", "9:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "full-pack-buggies": {
-    title: "Full Pack Buggies",
-    basePriceLabel: "From $109 USD",
-    adultPrice: 109,
-    childPrice: 65,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "full-pack-terracota": {
-    title: "Full Pack Terracota",
-    basePriceLabel: "From $119 USD",
-    adultPrice: 119,
-    childPrice: 70,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "splash-emotion": {
-    title: "Splash Emotion",
-    basePriceLabel: "From $89 USD",
-    adultPrice: 89,
-    childPrice: 55,
-    times: ["9:00 AM", "11:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "buggies-splash-emotion": {
-    title: "Buggies + Splash Emotion",
-    basePriceLabel: "From $129 USD",
-    adultPrice: 129,
-    childPrice: 75,
-    times: ["8:00 AM", "11:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "aqua-adrenaline-pack": {
-    title: "Aqua Adrenaline Pack",
-    basePriceLabel: "From $95 USD",
-    adultPrice: 95,
-    childPrice: 60,
-    times: ["9:00 AM", "11:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "aqua-adventure-pack": {
-    title: "Aqua Adventure Pack",
-    basePriceLabel: "From $85 USD",
-    adultPrice: 85,
-    childPrice: 55,
-    times: ["9:00 AM", "11:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "aqua-splash-pack": {
-    title: "Aqua Splash Pack",
-    basePriceLabel: "From $75 USD",
-    adultPrice: 75,
-    childPrice: 45,
-    times: ["9:00 AM", "11:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "domitai-park": {
-    title: "Domitai Park",
-    basePriceLabel: "From $89 USD",
-    adultPrice: 89,
-    childPrice: 55,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "fusion-zipline": {
-    title: "Fusion Zipline",
-    basePriceLabel: "From $95 USD",
-    adultPrice: 95,
-    childPrice: 60,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "fusion-horse": {
-    title: "Fusion Horseback",
-    basePriceLabel: "From $85 USD",
-    adultPrice: 85,
-    childPrice: 50,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "dolphin-encounter": {
-    title: "Dolphin Encounter",
-    basePriceLabel: "From $99 USD",
-    adultPrice: 99,
-    childPrice: 79,
-    times: ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "dolphin-swim": {
-    title: "Dolphin Swim",
-    basePriceLabel: "From $129 USD",
-    adultPrice: 129,
-    childPrice: 99,
-    times: ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-  "dolphin-royal-swim": {
-    title: "Dolphin Royal Swim",
-    basePriceLabel: "From $159 USD",
-    adultPrice: 159,
-    childPrice: 129,
-    times: ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "party-boat": {
-    title: "Party Boat",
-    basePriceLabel: "From $69 USD",
-    adultPrice: 69,
-    childPrice: 49,
-    times: ["10:00 AM", "1:00 PM", "4:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "safari-punta-cana": {
-    title: "Safari Punta Cana",
-    basePriceLabel: "From $65 USD",
-    adultPrice: 65,
-    childPrice: 40,
-    times: ["8:00 AM", "9:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "horseback-riding": {
-    title: "Horseback Riding",
-    basePriceLabel: "From $60 USD",
-    adultPrice: 60,
-    childPrice: 40,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "el-dorado-park": {
-    title: "El Dorado Park",
-    basePriceLabel: "From $70 USD",
-    adultPrice: 70,
-    childPrice: 45,
-    times: ["8:00 AM", "10:00 AM", "2:00 PM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "scape-park": {
-    title: "Scape Park",
-    basePriceLabel: "From $99 USD",
-    adultPrice: 99,
-    childPrice: 65,
-    times: ["8:00 AM", "10:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  },
-
-    "santo-domingo-city-tour": {
-    title: "Santo Domingo City Tour",
-    basePriceLabel: "From $85 USD",
-    adultPrice: 85,
-    childPrice: 55,
-    times: ["7:00 AM", "8:00 AM"],
-    hotels: [
-      "Hard Rock Hotel",
-      "RIU Republica",
-      "Barceló Bávaro",
-      "Meliá Caribe Beach",
-      "Majestic Colonial"
-    ]
-  }
-
-};
+function getTourDataById(tourId) {
+  if (!Array.isArray(MASTER_TOURS)) return null;
+  return MASTER_TOURS.find((tour) => tour.id === tourId) || null;
+}
 
 function renderBookingWidget() {
   const widget = document.getElementById("booking-widget");
   if (!widget) return;
 
   const tourKey = widget.dataset.tour;
-  const tour = toursData[tourKey];
+  const tour = getTourDataById(tourKey);
 
   if (!tour) {
     widget.innerHTML = "<p>Tour data not available.</p>";
@@ -539,7 +19,7 @@ function renderBookingWidget() {
     <div class="booking-card">
       <div class="booking-price-top">
         <span class="booking-price-label">From</span>
-        <strong>${tour.basePriceLabel.replace("From ", "")}</strong>
+        <strong>$${tour.adult} USD</strong>
         <small>Price per adult</small>
       </div>
 
@@ -552,7 +32,7 @@ function renderBookingWidget() {
         <div class="booking-field">
           <label for="booking-time">Start time</label>
           <select id="booking-time" class="booking-input">
-            ${tour.times.map(time => `<option value="${time}">${time}</option>`).join("")}
+            ${tour.times.map((time) => `<option value="${time}">${time}</option>`).join("")}
           </select>
         </div>
 
@@ -565,7 +45,12 @@ function renderBookingWidget() {
       <div class="booking-field">
         <label for="booking-hotel">Pickup hotel</label>
         <select id="booking-hotel" class="booking-input">
-          ${tour.hotels.map(hotel => `<option value="${hotel}">${hotel}</option>`).join("")}
+          <option value="">Select hotel</option>
+          <option value="Hard Rock Hotel">Hard Rock Hotel</option>
+          <option value="RIU Republica">RIU Republica</option>
+          <option value="Barceló Bávaro">Barceló Bávaro</option>
+          <option value="Meliá Caribe Beach">Meliá Caribe Beach</option>
+          <option value="Majestic Colonial">Majestic Colonial</option>
         </select>
       </div>
 
@@ -575,7 +60,7 @@ function renderBookingWidget() {
         <div class="booking-person-row">
           <div>
             <strong>Adults</strong>
-            <span>$${tour.adultPrice} USD each</span>
+            <span>$${tour.adult} USD each</span>
           </div>
           <div class="qty-control">
             <button type="button" class="qty-btn" data-type="adult" data-action="minus">−</button>
@@ -587,7 +72,7 @@ function renderBookingWidget() {
         <div class="booking-person-row">
           <div>
             <strong>Children</strong>
-            <span>$${tour.childPrice} USD each</span>
+            <span>$${tour.child} USD each</span>
           </div>
           <div class="qty-control">
             <button type="button" class="qty-btn" data-type="child" data-action="minus">−</button>
@@ -599,7 +84,7 @@ function renderBookingWidget() {
 
       <div class="booking-total-box">
         <span>Total</span>
-        <strong id="booking-total">$178 USD</strong>
+        <strong id="booking-total">$${tour.adult * 2} USD</strong>
       </div>
 
       <div class="booking-actions">
@@ -625,7 +110,7 @@ function renderBookingWidget() {
     adultCountEl.textContent = adults;
     childCountEl.textContent = children;
 
-    const total = adults * tour.adultPrice + children * tour.childPrice;
+    const total = adults * tour.adult + children * tour.child;
     totalEl.textContent = `$${total} USD`;
   }
 
@@ -658,12 +143,12 @@ function renderBookingWidget() {
 
   reserveBtn.addEventListener("click", () => {
     const selectedDate = dateEl.value || "not selected";
-    const selectedTime = timeEl.value;
-    const selectedHotel = hotelEl.value;
-    const total = adults * tour.adultPrice + children * tour.childPrice;
+    const selectedTime = timeEl.value || "not selected";
+    const selectedHotel = hotelEl.value || "not selected";
+    const total = adults * tour.adult + children * tour.child;
 
     const message =
-      `Hello PCG Tours, I want to reserve ${tour.title}. ` +
+      `Hello PCG Tours, I want to reserve ${tour.name}. ` +
       `Date: ${selectedDate}. ` +
       `Time: ${selectedTime}. ` +
       `Hotel: ${selectedHotel}. ` +
