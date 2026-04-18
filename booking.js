ca626a0e4686b806941757b67b0bd21cce0c826e
Azienflv <121687950+Azienflv@users.noreply.github.com>
2026-04-18 17:33:17 -0400

    Update booking.js

--- booking.js
+++ booking.js
@@ -1,4 +1,4 @@
-alert("booking.js loaded");
+
 
 const SUPABASE_URL = "https://gqurgezuuytxrcmudnik.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxdXJnZXp1dXl0eHJjbXVkbmlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTAyMjIsImV4cCI6MjA5MDE4NjIyMn0.1EW73snm3LvXPW0jK-g_-Klze0FyIbXI4dzv0J2XGr4";
